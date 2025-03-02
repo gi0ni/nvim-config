c6bd0a6d5ffd26d3dcd0218c641a72a33b1e4cae
Apetrei Ionuț-Teodor <apetreionuteodor@gmail.com>
2025-03-02 12:35:36 +0200

    Easier Esc with jk and got rid of auto comments on Enter.

--- init.lua
+++ init.lua
@@ -4,11 +4,17 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.nu = true
 
+-- quick escape --
+vim.api.nvim_set_keymap("i", "jk", "<esc>:noh<cr>", { noremap = true, silent = true });
+
 -- cursor blink --
 vim.opt.guicursor = { "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100" }
 
 -- netrw --
 vim.api.nvim_set_keymap("n", "<leader>e", ":Explore<cr>", { noremap = true, silent = true })
+
+-- comments --
+vim.api.nvim_create_autocmd("FileType", { pattern = "*", callback = function() vim.opt_local.formatoptions:remove({'c', 'r', 'o' }) end })
 
 -- search --
 vim.api.nvim_set_keymap("n", "<esc>", ":noh<cr>", { noremap = true, silent = true })
