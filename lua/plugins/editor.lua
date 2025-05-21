af20c05160df97cbaaa84ac078f39614a0a1e555
Apetrei Ionuț-Teodor <apetreionuteodor@gmail.com>
2025-05-21 07:45:20 +0300

    Added statusline.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -69,5 +69,10 @@ return
 		keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 		}
+	},
+
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {}
 	}
 }
