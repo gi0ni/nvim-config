dfd25dda0c5f2e480298bfb035d0d67078efb0bc
Apetrei Ionuț-Teodor <apetreionuteodor@gmail.com>
2025-05-27 15:21:15 +0300

    GLSL query.

---
+++ queries/glsl/highlights.scm
@@ -0,0 +1,37 @@
+; inherits: c
+
+[
+  "in"
+  "out"
+  "inout"
+  "uniform"
+  "shared"
+  "layout"
+  "attribute"
+  "varying"
+  "buffer"
+  "coherent"
+  "readonly"
+  "writeonly"
+  "precision"
+  "highp"
+  "mediump"
+  "lowp"
+  "centroid"
+  "sample"
+  "patch"
+  "smooth"
+  "flat"
+  "noperspective"
+  "invariant"
+  "precise"
+] @keyword.modifier
+
+"subroutine" @keyword.function
+
+(extension_storage_class) @keyword.modifier
+
+((identifier) @variable.builtin
+  (#lua-match? @variable.builtin "^gl_"))
+
+((identifier) @core_token (#eq? @core_token "core"))
