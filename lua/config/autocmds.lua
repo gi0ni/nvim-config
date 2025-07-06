697d10b4cb43df03436ea48e21734cab1ab296c7
apetrei_ionut <apetreionuteodor@gmail.com>
2025-07-06 22:01:10 +0300

    Forgot something.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,2 +1,2 @@
 -- disable automatic comment insertion on new lines --
-vim.api.nvim_create_autocmd("FileType", { pattern = "*", callback = function() vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' }) end })
+vim.api.nvim_create_autocmd('FileType', { pattern = '*', callback = function() vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' }) end })
