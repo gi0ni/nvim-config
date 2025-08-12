d93c7509a59fb3ca968d4d1f1badfd18168149e9
gi0ni <apetreionuteodor@gmail.com>
2025-08-12 18:59:46 +0300

    Automatically indenting blank lines should no longer print ^F in buffers without LSPs.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,28 +2,20 @@
 vim.api.nvim_create_autocmd('FileType', { pattern = '*', callback = function() vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' }) end })
 
 -- indent blank lines
-vim.api.nvim_create_autocmd('BufEnter', {
+vim.api.nvim_create_autocmd('LspAttach', {
 	pattern = '*',
-	callback = function()
-		vim.schedule(function()
-			if not vim.bo.indentkeys:find('%^F') or vim.bo.indentexpr == '' then
-				vim.keymap.set('n', 'a',      'a',      { silent = true })
-				vim.keymap.set('i', '<Up>',   '<Up>',   { silent = true })
-				vim.keymap.set('i', '<Down>', '<Down>', { silent = true })
-			else
-				vim.keymap.set('n', 'a', function()
-					return vim.fn.getline('.'):match('^%s*$') and 'a<C-f>' or 'a'
-				end, { expr = true, silent = true })
+	callback = function(args)
+		vim.keymap.set('n', 'a', function()
+			return vim.fn.getline('.'):match('^%s*$') and 'a<C-f>' or 'a'
+		end, { expr = true, silent = true, buffer = args.buf })
 
-				vim.keymap.set('i', '<Up>', function()
-					return vim.fn.getline(vim.fn.line('.') - 1):match('^%s*$') and '<Up><C-f>' or '<Up>'
-				end, { expr = true, silent = true })
+		vim.keymap.set('i', '<Up>', function()
+			return vim.fn.getline(vim.fn.line('.') - 1):match('^%s*$') and '<Up><C-f>' or '<Up>'
+		end, { expr = true, silent = true, buffer = args.buf })
 
-				vim.keymap.set('i', '<Down>', function()
-					return vim.fn.getline(vim.fn.line('.') + 1):match('^%s*$') and '<Down><C-f>' or '<Down>'
-				end, { expr = true, silent = true })
-			end
-		end)
+		vim.keymap.set('i', '<Down>', function()
+			return vim.fn.getline(vim.fn.line('.') + 1):match('^%s*$') and '<Down><C-f>' or '<Down>'
+		end, { expr = true, silent = true, buffer = args.buf })
 	end
 })
 
