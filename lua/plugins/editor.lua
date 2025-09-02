8f5a632e4a67b844fbf67f3503564e30d04d76bf
gi0ni <apetreionuteodor@gmail.com>
2025-09-02 16:05:45 +0300

    Added a plugin to make it easier to work with vim registers.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -82,6 +82,17 @@ return
 		end
 	},
 
+	{
+		"tversteeg/registers.nvim",
+		cmd = "Registers",
+		config = true,
+		keys = {
+			{ "\"",    mode = { "n", "v" } },
+			{ "<C-R>", mode = "i" }
+		},
+		name = "registers",
+	},
+
 
 	-- convinience
 	{
