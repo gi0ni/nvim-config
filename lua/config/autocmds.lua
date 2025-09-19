4dd95d881b73667ed2f57783720ffce13d7172b5
gi0ni <apetreionuteodor@gmail.com>
2025-09-19 21:16:28 +0300

    Add automatic detection for ^F indentation working or not. No need for a manual blacklist anymore.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,24 +8,20 @@ vim.api.nvim_create_autocmd('BufReadPost', { pattern = '*', callback = function(
 vim.api.nvim_create_autocmd('BufEnter', {
 	pattern = '*',
 	callback = function(args)
-		local blacklist = { '', 'text', 'NvimTree', 'asm', 's', 'ps1', 'markdown' }
-
-		for _, item in pairs(blacklist) do
-			if vim.bo.filetype == item then
-				return
-			end
+		if vim.bo.indentexpr == '' and vim.bo.cindent == false then
+			return
 		end
 
 		vim.keymap.set('n', 'a', function()
 			return vim.fn.getline('.'):match('^%s*$') and 'a<C-f>' or 'a'
-		end, { expr = true, silent = true, buffer = args.buf })
+		end, { expr = true, buffer = args.buf })
 
 		vim.keymap.set('i', '<Up>', function()
 			return vim.fn.getline(vim.fn.line('.') - 1):match('^%s*$') and '<Up><C-f>' or '<Up>'
-		end, { expr = true, silent = true, buffer = args.buf })
+		end, { expr = true, buffer = args.buf })
 
 		vim.keymap.set('i', '<Down>', function()
 			return vim.fn.getline(vim.fn.line('.') + 1):match('^%s*$') and '<Down><C-f>' or '<Down>'
-		end, { expr = true, silent = true, buffer = args.buf })
+		end, { expr = true, buffer = args.buf })
 	end
 })
