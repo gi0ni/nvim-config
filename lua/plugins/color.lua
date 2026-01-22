c3bee023291a914b66d38d76cfd1a2b7c058ecc6
gi0ni <apetreionuteodor@gmail.com>
2026-01-22 17:34:57 +0200

    Fix treesitter.

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -31,7 +31,7 @@ return
 		},
 
 		config = function(_, opts)
-			require('nvim-treesitter.configs').setup(opts)
+			require('nvim-treesitter.config').setup(opts)
 
 			vim.filetype.add({
 				extension = {
