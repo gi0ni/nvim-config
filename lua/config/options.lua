147f2129dcd0efeeabea225a7859d38afbc17399
gi0ni <apetreionuteodor@gmail.com>
2026-02-07 18:43:11 +0200

    stop indenting # to column 0

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop    = 4
 vim.opt.shiftwidth = 4
 vim.opt.cinscopedecls = ''
 vim.opt.cinkeys:remove(':')
+vim.opt.cinkeys:remove('0#')
 
 vim.opt.fillchars:append({ eob = ' ' })
 vim.opt.guicursor = 'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100'
