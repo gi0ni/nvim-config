2f19cc8afa89cdf805ca9ed065292e52ee6712c4
gi0ni <apetreionuteodor@gmail.com>
2026-04-16 12:53:50 +0300

    Enable relative line numbers.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.nu = true
--- vim.opt.relativenumber = true -- just use <x>gg to jump to line <x> lmao
+vim.opt.relativenumber = true -- You can also use <abs>gg to jump to a line.
 vim.diagnostic.config({ virtual_text = true })
 vim.opt.signcolumn = "no"
 vim.opt.shortmess:append('I')
