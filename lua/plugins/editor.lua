8a34ca9ee57447ae327c6477f226b0d2c1c47eb7
gi0ni <apetreionuteodor@gmail.com>
2026-07-04 13:09:03 +0300

    better-escape.nvim: remove jk escape from terminal mode

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -159,7 +159,39 @@ return
 		'max397574/better-escape.nvim',
 		config = function()
 			require('better_escape').setup {
-				timeout = 150
+				timeout = 150,
+				default_mappings = false, -- setting this to false removes all the default mappings
+				mappings = {
+					-- i for insert
+					i = {
+						j = {
+							-- These can all also be functions
+							k = "<Esc>",
+							j = "<Esc>",
+						},
+					},
+					c = {
+						j = {
+							k = "<C-c>",
+							j = "<C-c>",
+						},
+					},
+					-- t = {
+					-- 	j = {
+					-- 		k = "<C-\\><C-n>",
+					-- 	},
+					-- },
+					v = {
+						j = {
+							k = "<Esc>",
+						},
+					},
+					s = {
+						j = {
+							k = "<Esc>",
+						},
+					},
+				}
 			}
 		end
 	},
