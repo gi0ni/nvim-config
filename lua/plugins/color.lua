6c514c3601d1774f330e2424b2778308c21affee
gi0ni <apetreionuteodor@gmail.com>
2026-07-06 14:14:08 +0300

    Revamp 'color.lua'. Fix tree-sitter highlights not actually being used.

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -8,13 +8,13 @@ return
 			require("everforest").setup({
 				background = 'medium',
 			})
+
 			vim.cmd('colorscheme everforest')
-			vim.api.nvim_set_hl(0, 'ErrorMsg', { bold = true, fg = '#e67e80' })
-			vim.api.nvim_set_hl(0, 'cDefine',  { link = 'Include' })
-			vim.api.nvim_set_hl(0, 'String',   { link = 'Aqua' })
-			vim.api.nvim_set_hl(0, 'cSpecial', { link = 'Green' })
-			vim.api.nvim_set_hl(0, 'LspInlayHint', { fg = '#618d88' })
-		end,
+
+			local hl = require('utils.hl')
+			hl.set('ErrorMsg', {link='Red', underline=false})
+			hl.set('LspInlayHint', {link='Grey'})
+		end
 	},
 
 	{
@@ -25,25 +25,12 @@ return
 
 		config = function()
 			local site = require('nvim-treesitter')
+
 			site.setup({
 				install_dir = vim.fn.stdpath('data') .. '/site'
 			})
-			site.install({ 'html', 'glsl', 'python' })
 
-			vim.filetype.add({
-				extension = {
-					vert = 'glsl',
-					frag = 'glsl',
-					geom = 'glsl',
-					tesc = 'glsl',
-					tese = 'glsl',
-					comp = 'glsl'
-				}
-			})
-
-			vim.api.nvim_set_hl(0, '@core_token',          { link = '@keyword.directive.glsl' })
-			vim.api.nvim_set_hl(0, '@function.call.glsl',  { link = '@function.glsl' })
-			vim.api.nvim_set_hl(0, '@keyword.import.glsl', { link = 'PreProc' })
+			site.install({ 'glsl', 'python' })
 		end
 	}
 }
