73f707e49244d09b97496db28a18d3a0621aec2c
gi0ni <apetreionuteodor@gmail.com>
2026-07-08 21:35:12 +0300

    Complete 'build.lua' rewrite using the new Python script

--- lua/config/build.lua
+++ lua/config/build.lua
@@ -1,248 +1,206 @@
--- ask for args manually
-ArgsList = ''
+ArgsList = nil
 ArgsListTokenized = {}
-vim.keymap.set('n', '<leader>a', function()
+
+vim.keymap.set('n', '<leader>ba', function()
 	ArgsList = vim.fn.input('Enter Args: ')
 
-	-- copy by value
-	for key, _ in pairs(ArgsListTokenized) do
+	-- This is how you remove elements from a table in Lua...
+	for key in pairs(ArgsListTokenized) do
 		ArgsListTokenized[key] = nil
 	end
 
-	if ArgsList == '' then
+	if ArgsList == nil then
 		return
 	end
 
-	local temp = vim.split(ArgsList, ' +')
+	-- Important! Copy temporary table by value.
+	local tokens = vim.split(ArgsList, ' +')
 
-	for key, val in pairs(temp) do
+	for key, val in pairs(tokens) do
 		ArgsListTokenized[key] = val
 	end
 end)
 
+-- Quick build (and run) shortcuts for a wide variety of languages (4!)
+local pythonRuntime = IsWin32 and 'python' or 'python3'
+local globalBuildScript = vim.fn.stdpath('config') .. '/scripts/build.py'
 
--- cmake
-vim.keymap.set('n', '<leader>m', ':!cmake -B build -G Ninja<CR>')
+local curDirName
+local bufferName
+local binaryName
 
+local launchDisabled = false
 
--- build only
-vim.keymap.set('n', '<leader>n', ':Build<CR>')
-
-vim.api.nvim_create_user_command('Build', function()
-	vim.cmd('wa')
-
-	-- cmake
-	if vim.fn.isdirectory('build') ~= 0 then
-		vim.cmd('!ninja -C build')
-
-	-- rust
-	elseif vim.fn.filereadable('Cargo.toml') == 1 then
-		vim.cmd('!cargo build')
-
-	-- c single file
-	elseif vim.bo.filetype == 'c' then
-		local file = vim.fn.expand('%')
-		if vim.fn.isdirectory('bin') == 0 then vim.cmd('silent! !mkdir bin') end
-		local program = 'bin/' .. vim.fn.fnamemodify(file, ':r')
-		vim.cmd('!gcc -g ' .. file .. ' -o ' .. program)
-
-	-- unknown
-	else
-		vim.notify("failed to run build system. unknown project type")
-	end
-end, { desc = 'build. build. build.' })
-
-
--- build and run
 vim.keymap.set('n', '<leader>r', function()
-	Run(0)
+	Build({launch=true})
 end)
 
-vim.keymap.set('n', '<F5>', function()
-	local found = false
-	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-		local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype');
-		found = (filetype == 'c')
-	end
+vim.keymap.set('n', '<leader>bb', function()
+	Build({launch=false})
+end)
 
-	if not found then
-		vim.notify('only works with single file c applets')
+vim.keymap.set('n', '<leader>bm', ':!cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=Debug')
+
+vim.keymap.set('n', '<leader>bpy', function()
+	if vim.fn.filereadable('build.py') == 1 then
+		vim.notify('There already is a local `build.py`. Will not overwrite.')
 		return
 	end
 
-	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-		Run(bufnr)
+	local inpfile = io.open(globalBuildScript, 'r')
+	local outfile = io.open('build.py', 'w')
+
+	if inpfile == nil or outfile == nil then
+		vim.notify('Error. Failed to copy file!')
+		return
 	end
+
+	local contents = inpfile:read('*a')
+	inpfile:close()
+
+	outfile:write(contents)
+	outfile:close()
+	vim.notify(string.format('Copied `%s` to local directory.', vim.fn.stdpath('config') .. '/scripts/build.py'))
 end)
 
-function Run(bufnr)
+function Build(opt)
+	launchDisabled = not opt.launch
+
 	vim.cmd('wa')
 
-	local file = vim.api.nvim_buf_get_name(bufnr)
-	local filetype = vim.api.nvim_buf_get_option(bufnr, 'filetype');
-
-	-- build script
-	if vim.fn.filereadable('build.sh') == 1 then
-		vim.cmd("silent! !bash build.sh "..ArgsList)
-
-	-- cmake
-	elseif vim.fn.isdirectory('build') ~= 0 then
-		Launch("ninja -C build", "bin", "buildsystem")
-
-	-- rust
-	elseif vim.fn.filereadable('Cargo.toml') == 1 then
-		Launch("cargo build", "target/debug", "buildsystem")
-
-	-- java
-	elseif vim.fn.filereadable('src/Program.java') == 1 then
-		Launch("javac -d bin -g src/*.java", "java -cp bin Program", "jit")
-
-	-- python
-	elseif filetype == "python" then
-		Launch((IsWin32 and "python" or "python3"), file, "interpreter")
-
-		-- javascript
-	elseif filetype == "javascript" or filetype == "typescript" then
-		Launch("node", file, "interpreter")
-
-		-- bash scripts
-	elseif not IsWin32 and filetype == 'sh' then
-		LaunchLinux("bash", file, "interpreter")
-
-		-- c single file
-	elseif filetype == 'c' then
-		if vim.fn.isdirectory("bin") == 0 then vim.cmd("silent! !mkdir bin") end
-		local program = "bin/" .. vim.fn.fnamemodify(file, ":t:r")
-		Launch("gcc -g " .. file .. " -o " .. program, program, "compiler")
-
-		-- unknown
-	else
-		vim.notify("failed to run '" .. file .. "'. unknown file type")
+	if vim.fn.filereadable('build.py') == 1 then
+		RunBuildScript(nil, nil, 'build.py')
+		return
 	end
+
+	curDirName = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+	bufferName = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':t')
+	binaryName = curDirName .. (IsWin32 and '.exe' or '')
+
+	local config = ResolveBuilder()
+	if config == nil then
+		vim.notify(string.format('Failed to launch `%s`. No configuration found.', bufferName))
+		return
+	end
+
+	if config.build == nil and (config.launch == nil or launchDisabled) then
+		vim.notify('No commands were given. There is nothing to do.')
+		return
+	end
+
+	RunBuildScript(config.build, config.launch)
 end
 
-function Launch(build, run, mode) -- mode can be buildsystem, compiler and interpreter
-	if IsWin32 then
-		LaunchWindows(build, run, mode)
-	else
-		LaunchLinux(build, run, mode)
-	end
-end
+local builders = {
+	CMAKE  = 1,
+	CARGO  = 2,
+	PYTHON = 3,
+	BASH   = 4
+}
 
-
---==================== WINDOWS ====================--
-function LaunchWindows(build, run, mode)
-
-	if mode == "buildsystem" then
-		-- add compile command
-		build = string.format([[
-			%s
-			Write-Host ''
-		]], build)
-
-		-- deduct executable path
-		local cwd     = vim.fn.getcwd()
-		local program = vim.fn.fnamemodify(cwd, ":t")
-
-		run = string.format([[
-			& %s/%s.exe %s
-		]], run, program, ArgsList)
-
-	elseif mode == "interpreter" then
-		run = build .. " " .. run .. " " .. ArgsList
-		build = "cmd /c exit 0"
-
-	elseif mode == "compiler" then
-		run = "& ./" .. run .. " " .. ArgsList
-
-	else
-		vim.notify("unknown launch mode '" .. mode .. "'!")
-	end
-
-	local command = string.format([[
-		<# build #>
-		%s
-
-		<# run #>
-		$timer = [System.Diagnostics.Stopwatch]::new()
-
-		if($LastExitCode -eq 0) {
-			$timer.Start()
-			%s
+local builderConfig = {
+	[builders.CMAKE] = {
+		build = 'ninja -C build',
+		launch = 'bin/{binaryName} {ArgsList}',
+		pattern = {
+			type = 'file',
+			what = 'CMakeLists.txt'
 		}
+	},
 
-		<# error code #>
-		Write-Host ''
-		Write-Host ''
-		Write-Host ''
-		Write-Host -NoNewLine ('Process returned code {0} (0x{1})' -f $LASTEXITCODE, ($LASTEXITCODE).ToString('X').PadLeft(8, '0'))
+	[builders.CARGO] = {
+		build = 'cargo build',
+		launch = 'target/debug/{binaryName} {ArgsList}',
+		pattern = {
+			type = 'file',
+			what = 'Cargo.toml'
+		}
+	},
 
-		<# time #>
-		$timer.Stop()
-		$minutes      = $timer.Elapsed.Minutes
-		$seconds      = $timer.Elapsed.Seconds
-		$milliseconds = $timer.Elapsed.Milliseconds
-		Write-Host (' in {0:d2}:{1:d2}.{2:d3} seconds.' -f $minutes, $seconds, $milliseconds)
+	[builders.PYTHON] = {
+		build = nil,
+		launch = '{pythonRuntime} {bufferName} {ArgsList}',
+		pattern = {
+			type = 'extension',
+			what = 'py'
+		}
+	},
 
-		<# pause #>
-		Write-Host -NoNewLine 'Press any key to continue...'
-		$null = [System.Console]::ReadKey()
-	]], build, run):gsub("[\n\t]+", "\\;")
+	[builders.BASH] = {
+		build = nil,
+		launch = 'bash {bufferName} {ArgsList}',
+		pattern = {
+			type = 'extension',
+			what = 'sh'
+		}
+	}
+}
 
-	vim.fn.jobstart([[ wt -p "PowerShell" --startingDirectory "." pwsh -c "]] .. command .. [["]])
-end
+function ResolveBuilder()
+	local orderedKeys = {}
 
-
---==================== LINUX ====================--
-function LaunchLinux(build, run, mode)
-
-	local command_b = [[]]
-
-	if mode == "buildsystem" then
-		-- get binary path
-		local cwd     = vim.fn.getcwd()
-		local program = vim.fn.fnamemodify(cwd, ":t")
-
-		run = string.format([[
-			./%s/%s
-		]], run, program)
-
-		command_b = string.format([[ %s && (echo; %s %s);]], build, run, ArgsList);
-
-	elseif mode == "compiler" then
-		command_b = string.format([[ %s && (./%s %s); ]], build, run, ArgsList)
-
-	elseif mode == "jit" then
-		command_b = string.format([[ %s && (%s %s); ]], build, run, ArgsList)
-
-	elseif mode == "interpreter" then
-		command_b = string.format([[ %s %s %s; ]], build, run, ArgsList) -- e.g. python program.py
-
-	else
-		vim.notify("unknown launch mode '" .. mode .. "'!")
+	for key in pairs(builders) do
+		table.insert(orderedKeys, key)
 	end
 
-	local command_a = [[
-		start=$(date +%s%3N);
-	]]
+	table.sort(orderedKeys)
 
-	local command_c = [[
-		ret=$?;
+	local chosenKey = nil
 
-		end=$(date +%s%3N);
-		duration_ms=$((end - start));
-		minutes=$((duration_ms / 60000));
-		seconds=$(( (duration_ms % 60000) / 1000 ));
-		millis=$((duration_ms % 1000));
-		formatted_time=$(printf "%02d:%02d.%03d" $minutes $seconds $millis);
+	for key = 1, #orderedKeys do
+		if MatchBuilderPattern(builderConfig[key].pattern) then
+			chosenKey = key
+			break
+		end
+	end
 
-		echo -ne "\n\n\n";
-		echo -ne "Process returned code $ret (0x$(printf "%08X" $ret))";
-		echo " in $formatted_time seconds.";
-		echo -ne "Press any key to continue...";
-		read -n 1;
-	]]
+	if chosenKey == nil then
+		return nil
+	end
 
-	local final_command = command_a .. command_b .. command_c
-	vim.fn.jobstart("tmux new-window -n build '" .. final_command .. "'")
+	return GetBuilderCommands(builderConfig[chosenKey])
+end
+
+function MatchBuilderPattern(pattern)
+	if pattern.type == 'file' then
+		return vim.fn.filereadable(pattern.what) == 1
+
+	elseif pattern.type == 'extension' then
+		local ext = vim.fn.fnamemodify(bufferName, ':e')
+		return ext == pattern.what
+	end
+end
+
+function GetBuilderCommands(config)
+	local fmtArgs = {
+		['{binaryName}'] = binaryName,
+		['{ArgsList}'] = ArgsList or '',
+		['{pythonRuntime}'] = pythonRuntime,
+		['{bufferName}'] = bufferName,
+	}
+
+	local regexPattern = '{[a-zA-Z_][0-9a-zA-Z_]*}'
+
+	return {
+		build = config.build and string.gsub(config.build, regexPattern, fmtArgs) or nil,
+		launch = config.launch and string.gsub(config.launch, regexPattern, fmtArgs) or nil
+	}
+end
+
+function RunBuildScript(buildCmd, launchCmd, buildScript)
+	buildScript = buildScript or globalBuildScript
+
+	local cmd = {pythonRuntime, buildScript}
+
+	if buildCmd ~= nil then
+		table.insert(cmd, '--build')
+		table.insert(cmd, buildCmd)
+	end
+
+	if launchCmd ~= nil then
+		table.insert(cmd, '--launch')
+		table.insert(cmd, launchCmd)
+	end
+
+	vim.fn.jobstart(cmd)
 end
