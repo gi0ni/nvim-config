7a9dafd981828584a4451321ebf6304ae93cd40a
gi0ni <apetreionuteodor@gmail.com>
2026-07-08 21:43:20 +0300

    Re-enable some options such as the startup screen and tildas at end of buffer.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,15 +1,24 @@
-vim.opt.shortmess:append('I') -- Disable start screen.
-vim.opt.fillchars:append({ eob = ' ' }) -- Do not display ~ after end of buffers.
-vim.opt.guicursor = "n-v-c:block-Cursor"
-
 vim.opt.relativenumber = true
 vim.opt.nu = true
+vim.opt.guicursor = "n-v-c:block-Cursor"
 
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({virtual_text = true})
 vim.opt.signcolumn = "no"
+vim.opt.foldenable = false
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.cinkeys:remove('0#') -- Disable auto indent for keywords like 'public:' and # directives.
+vim.opt.cinkeys:remove('0#') -- Disable auto indent for 'public:', '#ifdef' and etc.
 vim.opt.cinkeys:remove(':')
 vim.opt.cinscopedecls = ''
+
+vim.filetype.add({
+	extension = {
+		vert = 'glsl',
+		frag = 'glsl',
+		geom = 'glsl',
+		tesc = 'glsl',
+		tese = 'glsl',
+		comp = 'glsl'
+	}
+})
