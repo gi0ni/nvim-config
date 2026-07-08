ac54506c5d5ec600332a2fca3731c7a526ffd4f9
gi0ni <apetreionuteodor@gmail.com>
2026-07-08 22:27:56 +0300

    Cleaned up 'debugger.lua' a little bit. It will need to be touched upon more later

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,7 +1,7 @@
 -- FIX:
--- 1. windows/lldb: prints harmless warning about 'feature ignored'. prob because of exceptions
--- 3. linux/cppdbg: gdb prints harmless(?) warnings too. 'gdb failed to set controlling terminal'
--- 4. refocus neovim
+-- 1. Fix debugger disconnecting when an exception happens. That's the entire point of the debugger!!
+-- * Windows/lldb: Prints harmless warning about 'feature ignored'. Prob because of exceptions
+-- * Linux/cppdbg: gdb prints harmless(?) warnings too. 'gdb failed to set controlling terminal'
 
 return
 {
@@ -10,31 +10,24 @@ return
 		lazy = true,
 
 		keys = {
-			{ '<leader>d', function()
-				vim.cmd('Build')
+			{ '<leader>d', ":DapNew<CR>", silent=true },
 
-				if vim.v.shell_error == 0 then
-					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'm', false)
-					vim.cmd('DapNew')
-				end
-			end, silent = true },
+			{ '<leader>db', ':lua require("dap").toggle_breakpoint()<CR>', silent=true },
+			{ '<leader>dB', ':DapClearBreakpoints<CR>', silent=true },
+			{ '<leader>dw', ':DapViewWatch<CR>', silent=true },
 
-			{ '<leader>b', ':lua require("dap").toggle_breakpoint()<CR>', silent = true },
-			{ '<leader>B', ':DapClearBreakpoints<CR>', silent = true },
-			{ '<leader>w', ':DapViewWatch<CR>', silent = true },
+			{ '<leader>d1', ':DapStepOver<CR>', silent=true },
+			{ '<leader>d2', ':DapStepInto<CR>', silent=true },
+			{ '<leader>d3', ':DapStepOut<CR>', silent=true },
+			{ '<leader>d4', ':DapContinue<CR>', silent=true },
+			{ '<leader>d`', ':DapTerminate<CR>', silent=true },
 
-			{ '1', function() return require('dap').session() ~= nil and ':DapStepOver<CR>'  or '1' end, expr = true, silent = true },
-			{ '2', function() return require('dap').session() ~= nil and ':DapStepInto<CR>'  or '2' end, expr = true, silent = true },
-			{ '3', function() return require('dap').session() ~= nil and ':DapStepOut<CR>'   or '3' end, expr = true, silent = true },
-			{ '4', function() return require('dap').session() ~= nil and ':DapContinue<CR>'  or '4' end, expr = true, silent = true },
-			{ '`', function() return require('dap').session() ~= nil and ':DapTerminate<CR>' or '`' end, expr = true, silent = true },
-
-			{ 'W', function() return require('dap').session() ~= nil and ':DapViewJump watches<CR>'     or 'W' end, expr = true, silent = true },
-			{ 'S', function() return require('dap').session() ~= nil and ':DapViewJump scopes<CR>'      or 'S' end, expr = true, silent = true },
-			{ 'E', function() return require('dap').session() ~= nil and ':DapViewJump exceptions<CR>'  or 'E' end, expr = true, silent = true },
-			{ 'B', function() return require('dap').session() ~= nil and ':DapViewJump breakpoints<CR>' or 'B' end, expr = true, silent = true },
-			{ 'T', function() return require('dap').session() ~= nil and ':DapViewJump threads<CR>'     or 'T' end, expr = true, silent = true },
-			{ 'R', function() return require('dap').session() ~= nil and ':DapViewJump repl<CR>'        or 'R' end, expr = true, silent = true },
+			{ '<leader>dW', ':DapViewJump watches<CR>', silent=true },
+			{ '<leader>dS', ':DapViewJump scopes<CR>', silent=true },
+			{ '<leader>dE', ':DapViewJump exceptions<CR>', silent=true },
+			{ '<leader>dB', ':DapViewJump breakpoints<CR>', silent=true },
+			{ '<leader>dT', ':DapViewJump threads<CR>', silent=true },
+			{ '<leader>dR', ':DapViewJump repl<CR>', silent=true },
 		},
 
 		config = function()
@@ -50,7 +43,7 @@ return
 				dap.defaults.fallback.force_external_terminal = true
 			end
 
-			-- ========== c/cpp ========== --
+			-- ##### C/C++ #####
 			dap.adapters.cppdbg = {
 				id = 'cppdbg',
 				type = 'executable',
@@ -68,11 +61,8 @@ return
 
 					cwd = '${workspaceFolder}',
 					program = function()
-						if vim.fn.filereadable('CMakeLists.txt') == 1 then
-							return '${workspaceFolder}/bin/${workspaceFolderBasename}' .. (IsWin32 and '.exe' or '')
-						else
-							return 'bin/' .. vim.fn.fnamemodify(vim.fn.expand('%'), ':r') .. (IsWin32 and '.exe' or '') -- this is for single file c
-						end
+						local binaryPath = vim.fn.input("Binary Path: ./")
+						return '${workspaceFolder}/' .. binaryPath
 					end,
 					args = ArgsListTokenized,
 
@@ -81,9 +71,9 @@ return
 			}
 
 			dap.configurations.c = dap.configurations.cpp
-			-- =========================== --
 
-			-- ========== rust =========== --
+
+			-- ##### Rust #####
 			dap.adapters.lldb = {
 				type = 'server',
 				port = '${port}',
@@ -101,22 +91,24 @@ return
 					request = 'launch',
 
 					cwd = '${workspaceFolder}',
-					program = '${workspaceFolder}/target/debug/${workspaceFolderBasename}' .. (IsWin32 and '.exe' or ''),
+					program = function()
+						local binaryPath = vim.fn.input("Binary Path: ./")
+						return '${workspaceFolder}/' .. binaryPath
+					end,
 
 					stopOnEntry = false,
 					terminal = (IsWin32 and 'console' or 'external'),
 					console = 'externalTerminal'
 				}
 			}
-			-- =========================== --
 
 
-			-- open and close dap-view automatically
+			-- Open and close dap-view automatically
 			dap.listeners.before.launch['dap_view_open'] = function()
 				require('dap-view').setup()
 				vim.cmd('DapViewOpen')
 
-				-- start with locals tab open by default
+				-- Start with the locals tab open by default
 				vim.cmd('wincmd j')
 				if vim.bo.filetype == 'dap-view-term' then
 					vim.cmd('bd')
@@ -125,12 +117,7 @@ return
 
 				vim.api.nvim_feedkeys('S', 'c', false)
 
-				-- refocus neovim
-				-- if IsWin32 then
-				-- 	vim.cmd([[ silent !pwsh -Command "& /scripts/minimize_window.ps1" ]])
-				-- end
-
-				-- close adapter disconnect notification
+				-- Hide adapter disconnect notification
 				local original_notify = vim.notify
 				vim.notify = function(msg, level, opts)
 					if msg:match('cppdbg') then
@@ -145,7 +132,7 @@ return
 			end
 
 
-			-- highlight lines that have breakpoints
+			-- Custom highlight for lines that have breakpoints placed on them
 			vim.api.nvim_set_hl(0, 'DapBreakpointLine', { bg = '#fb4934', fg = '#ebdbb2' })
 			vim.fn.sign_define('DapBreakpoint', {
 				linehl = 'DapBreakpointLine',
