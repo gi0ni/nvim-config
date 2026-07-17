c3b6f3d4826ca729fa9669601875f3d1df97f5a2
gi0ni <apetreionuteodor@gmail.com>
2026-07-17 09:49:38 +0300

    Add GLSL highlight rules for missing statements

--- after/queries/glsl/highlights.scm
+++ after/queries/glsl/highlights.scm
@@ -1,8 +1,13 @@
 ;; extends
+; NOTE: You can use :InspectTree from the editor
 
 ((identifier) @core.token
 	(#has-ancestor? preproc_call)
 	(#eq? @core.token "core")
+)
+
+((identifier) @discard.token
+			  (#eq? @discard.token "discard")
 )
 
 ; ((layout_specification) @layout.specifier
