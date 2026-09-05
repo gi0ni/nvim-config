94874a28fede93eb4dc0bcbb99e5ff41eeefa795
gi0ni <apetreionuteodor@gmail.com>
2026-09-05 19:09:27 +0300

    Change blink.cmp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -66,15 +66,14 @@ return
 		opts = {
 			keymap = {
 				preset = "none",
-				["<C-r>"] = {"show", "hide", "fallback"},
-				["<C-e>"] = {"select_and_accept", "fallback"},
+				["<C-e>"] = {"show", "hide", "fallback"},
 				["<M-g>"] = {"show_documentation", "hide_documentation", "fallback"},
 				["<C-f>"] = {"scroll_documentation_up", "scroll_signature_up", "fallback"},
 				["<C-b>"] = {"scroll_documentation_down", "scroll_signature_down", "fallback"},
 				["<C-n>"] = {"select_next", "fallback"},
 				["<C-p>"] = {"select_prev", "fallback"},
 				["<C-k>"] = {"show_signature", "hide_signature", "fallback"},
-				["<Tab>"] = {"snippet_forward", "fallback"},
+				["<Tab>"] = {"select_and_accept", "snippet_forward", "fallback"},
 				["<S-Tab>"] = {"snippet_backward", "fallback"}
 			},
 			appearance = {
