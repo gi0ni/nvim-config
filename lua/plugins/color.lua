32da62499275635c9a90cf789f49212d3a05636c
gi0ni <apetreionuteodor@gmail.com>
2026-09-09 15:24:26 +0300

    Make special characters use the koda-moss special color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,3 +1,31 @@
+local palette = {
+	bg         = "#0e1415",
+	fg         = "#cecece",
+	dim        = "#30464a",
+	line       = "#182325",
+	keyword    = "#999999",
+	type       = "#708b8d",
+	operator   = "#708b8d",
+	comment    = "#707070",
+	border     = "#cecece",
+	emphasis   = "#71ade7",
+	func       = "#71ade7",
+	string     = "#95cb82",
+	char       = "#dfdf8e",
+	special    = "#dfdf8e",
+	const      = "#dfdf8e",
+	highlight  = "#cd974b",
+	info       = "#8ebeec",
+	success    = "#6abf40",
+	warning    = "#ec8013",
+	danger     = "#d2322d",
+	green      = "#95cb82",
+	orange     = "#ec8013",
+	red        = "#c33c33",
+	pink       = "#cc8bc9",
+	cyan       = "#47bea9",
+}
+
 return
 {
 	{
@@ -12,12 +40,13 @@ return
 					}
 				},
 				on_highlights = function(hl, c)
+					hl.Special = {fg=c.special}
+
 					hl.DiagnosticUnderlineHint = {undercurl=true, underline=false}
 					hl.DiagnosticUnderlineWarn = {undercurl=true, underline=false}
 					hl.DiagnosticUnderlineError = {undercurl=true, underline=false}
 
 					hl.Comment = {fg=c.comment, italic=true}
-
 					hl.TodoBgPERF = {bg=c.green, fg=c.bg, bold=true}
 					hl.TodoFgPERF = {fg=c.green}
 					hl.TodoBgTODO = {bg=c.pink, fg=c.bg, bold=true}
